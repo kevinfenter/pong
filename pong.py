1604a4a53328fe023396c4a6e73561ebe2c2c963
Kevin Fenter <69165709+kevinfenter@users.noreply.github.com>
2021-06-24 13:05:31 -0400

    Update pong.py
    added borders

--- pong.py
+++ pong.py
@@ -69,3 +69,23 @@ wn.onkeypress(pb_down, "k")
 
 while True:
     wn.update()
+
+# borders
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
